ffcc28a1543380d7e1ff67eb4832f67f2b0aabe8
rakshinaidu <2004rakshi@gmail.com>
2025-07-28 10:34:47 +0530

    Update db_start.py

--- python_rakshi/db_start.py
+++ python_rakshi/db_start.py
@@ -1,2 +1,12 @@
 from db_methods import insert_data
 insert_data(5,"Raksh",1234,"rs@gmail.com")
+
+from db_methods import delete_data 
+delete_data(75)
+
+from db_methods import update_data
+update_data("Rakshu",55)
+
+from db_methods import select_data
+result=select_data()
+print(result)
