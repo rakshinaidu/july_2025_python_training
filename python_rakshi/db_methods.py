4407e81f8a773122720c22aa6b893e5fb00f0575
rakshinaidu <2004rakshi@gmail.com>
2025-07-28 10:35:59 +0530

    Update db_methods.py

--- python_rakshi/db_methods.py
+++ python_rakshi/db_methods.py
@@ -1,26 +1,5 @@
-# import mysql.connector
-# def insert_data(id,name,password,email):
-#     mydb = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="rakshi@05",
-#     database="rakshitha_cse"
-
-#     )
-#     cur=mydb.cursor()
-#     sql="INSERT INTO user (id,name,password,email) VALUES (%s,%s,%s,%s)"
-#     val=(id,name,password,email)
-#     cur.execute(sql,val)
-#     mydb.commit()
-#     cur.close()
-   
-#     mydb.close()
-#     print("RECORD INSERTED.")
-
-
-# def updateData():
 import mysql.connector
-def updat_data(id,name,password,email):
+def insert_data(id,name,password,email):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -39,8 +18,64 @@ def updat_data(id,name,password,email):
     print("RECORD INSERTED.")
 
 
-# def deleteData():
+import mysql.connector
+def update_data(name,id):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="rakshi@05",
+    database="rakshitha_cse"
 
-# def selectData():
+    )
+    cur=mydb.cursor()
+    cur.execute("update user set name=%s where id=%s",(name,id))
+    mydb.commit()
+    cur.close()
+    mydb.close()
+    print("RECORD UPDATED.")
+
+
+def deleteData():
+import mysql.connector
+def delete_data(id):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="rakshi@05",
+    database="rakshitha_cse"
+
+    )
+    cur=mydb.cursor()
+    sql="delete from user where id=%s"
+    val=(id,)
+    cur.execute(sql,val)
+    mydb.commit()
+    cur.close()
+   
+    mydb.close()
+    print("RECORD DELETED.")
+
+
+import mysql.connector
+def select_data():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="rakshi@05",
+    database="rakshitha_cse"
+
+    )
+    cur=mydb.cursor()
+    sql="select * from user"
+    cur.execute(sql)
+    result=cur.fetchall()
+    mydb.commit()
+    cur.close()
+    mydb.close()
+    return result
+  
+
+
+
 
     
